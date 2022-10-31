4e3fd79ffb156993049103a7cac264e194dca35c
Gaurish <gaurishbaliga@gmail.com>
2022-10-31 20:42:24 +0530

    add A - Frog 1

---
+++ A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    
+    int a[n], dp[n];
+
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    dp[0] = 0; dp[1] = abs(a[0] - a[1]);
+
+    for(int i = 2; i < n; i++)
+    {
+        dp[i] = min(dp[i-1] + abs(a[i] - a[i-1]), dp[i-2] + abs(a[i] - a[i-2]));
+    }
+
+    cout << dp[n-1] << "\n"; 
+}
